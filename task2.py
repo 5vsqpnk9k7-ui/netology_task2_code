d6520dbd4c6036e3f4989cf8ec69eabb33b9451b
5vsqpnk9k7-ui <5vsqpnk9k7@privaterelay.appleid.com>
2026-03-12 21:16:08 +0300

    Add files via upload

--- task2.py
+++ task2.py
@@ -36,29 +36,33 @@ df_task2_part1 = (df
 2. Top 10 стран с максимальным зафиксированным кол-вом новых случаев за последнюю неделю марта 2021 в отсортированном порядке по убыванию
 (в выходящем датасете необходимы колонки: число, страна, кол-во новых случаев)
 """
+windowSpecPart2 = Window.partitionBy("location").orderBy(F.col("new_cases").desc(), F.col("date").asc())
+
 df_task2_part2 = (df
     .filter(F.col("continent").isNotNull())
     .filter(F.col("date").between("2021-03-25", "2021-03-31"))
-    .select(F
-            .col("date").alias("число"),
-            F.col("location").alias("страна"),
-            F.col("new_cases").alias("кол-во новых случаев")
+    .withColumn("rn", F.row_number().over(windowSpecPart2))
+    .filter(F.col("rn") == 1)
+    .select(
+        F.col("date").alias("число"),
+        F.col("location").alias("страна"),
+        F.col("new_cases").alias("кол-во новых случаев")
     )
+    # 5. Сортируем уже сами страны между собой и берем ТОП-10
     .orderBy(F.col("кол-во новых случаев").desc())
     .limit(10)
 )
-
 """
 3. Посчитайте изменение случаев относительно предыдущего дня в России за последнюю неделю марта 2021. 
 (например: в россии вчера было 9150 , сегодня 8763, итог: -387) (в выходящем датасете необходимы колонки: 
 число, кол-во новых случаев вчера, кол-во новых случаев сегодня, дельта).
 """
-windowSpec = Window.partitionBy("location").orderBy("date")
+windowSpecPart3 = Window.partitionBy("location").orderBy("date")
 
 df_task2_part3 = (df
     .filter(F.col("location") == "Russia")
     .filter(F.col("date").between("2021-03-24", "2021-03-31"))
-    .withColumn("вчера", F.lag("new_cases").over(windowSpec))
+    .withColumn("вчера", F.lag("new_cases").over(windowSpecPart3))
     .withColumn("дельта", F.col("new_cases") - F.col("вчера"))
     .filter(F.col("date") >= "2021-03-25")
     .select(
